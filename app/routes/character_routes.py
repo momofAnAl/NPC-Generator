71792d3ac23ffbbcb10234cf497d451364ca0986
anhtran <anhtr077@gmail.com>
2024-12-09 09:40:29 -0800

    update the generate_greeting and add_greetings function

--- app/routes/character_routes.py
+++ app/routes/character_routes.py
@@ -3,7 +3,10 @@ from ..db import db
 from ..models.character import Character
 from ..models.greeting import Greeting
 from sqlalchemy import func, union, except_
+import google.generativeai as genai
+import os
 
+genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
 bp = Blueprint("characters", __name__, url_prefix="/characters")
 
 @bp.post("")
@@ -46,10 +49,40 @@ def get_greetings(char_id):
 
 @bp.post("/<char_id>/generate")
 def add_greetings(char_id):
-    pass
+    character_obj = validate_model(Character, char_id)
+    greetings = generate_greetings(character_obj)
+
+    if character_obj.greetings:
+        return {"message": f"Greetings already generated for {character_obj.name} "}, 201
+    
+    new_greetings = []
+
+    for greeting in greetings:
+        new_greeting = Greeting(
+            greeting_text = greeting.strip("\""), #Removes quotes from each string
+            character = character_obj
+        )
+        new_greetings.append(new_greeting)
+    
+    db.session.add_all(new_greetings)
+    db.session.commit()
+
+    return {"message": f"Greetings successfully added to {character_obj.name}"}, 201
 
 def generate_greetings(character):
-    pass
+    character = validate_model(Character, char_id)
+    
+    if not character.greetings:
+        return {"message": f"No greetings found for {character.name} "}, 201
+    
+    response = {"Character Name" : character.name,
+                "Greetings" : []}
+    for greeting in character.greetings:
+        response["Greetings"].append({
+            "greeting" : greeting.greeting_text
+        })
+    
+    return response
 
 def validate_model(cls,id):
     try:
